808d1a533ae47492ef8a557bba9ec02c3ddb8aa9
Prasanna Rajaram <prasanna.uralsk@gmail.com>
2026-05-28 12:41:15 +0400

    Updated port informtion on server.js

--- server.js
+++ server.js
@@ -49,5 +49,5 @@ io.on("connection", (socket) => {
 const PORT = process.env.PORT || 3000;
 
 server.listen(PORT, () => {
-  console.log("Server running on port ",$PORT);
+  console.log("Server running on port ",PORT);
 });
